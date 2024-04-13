c73de242ab285ee44f27ec2aad025e3fa2d03af4
Abhi <abhishek0068.be21@chitkara.edu.in>
2024-04-13 14:09:39 +0530

    mostly completed

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { useContext, useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Register from './componenets/regitser/Register'
+import Login from './componenets/login-auth/Login'
+import NotFound from './NotFound'
+import Track from './componenets/Track'
+import { useNavigate } from 'react-router-dom'
+import { UserContext } from './context/UserContext'
+import Private from './componenets/Private'
+import ForgetPassword from './componenets/login-auth/ForgetPassword'
+import LoginOtp from './componenets/login-auth/LoginOtp'
+
+function App() {
+  const [loggedUser,setLoggedUser] = useState(JSON.parse(localStorage.getItem("token")));
+  // const navigate = useNavigate();
+  return (
+    <>
+    <UserContext.Provider value = {{loggedUser,setLoggedUser}}>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element = {<Private Component = {Track}/>}/>
+        <Route path='/register' element = {<Register/>}/>
+        <Route path='/login' element = {<Login/>}/>
+        <Route path='/forgetPassword' element= {<ForgetPassword/>}/>
+        <Route path='/LoginOtp' element={<LoginOtp/>}/>
+        <Route path='/track' element = {<Private Component = {Track}/>}/>
+        <Route path='*' element = {<NotFound/>}/>
+      </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
+    </>
+  )
+}
+
+export default App
